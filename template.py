0b6997a98a8a8262c88e9f9c64bf84783d7c2c98
juinnsheng <116355442+juinnsheng@users.noreply.github.com>
2023-08-11 16:11:28 +0800

    Model Evaluation added

--- template.py
+++ template.py
@@ -17,7 +17,7 @@ list_of_files = [ #Creating folders
     f"src/{project_name}/entity/__init__.py",
     f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
-    "dvc.yaml" #ML tools
+    "dvc.yaml", #ML tools
     "params.yaml",
     "requirements.txt",
     "setup.py",
